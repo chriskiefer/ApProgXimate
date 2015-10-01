412b6cf7cfe90db334680723453d4267ee2195c7
Chris Kiefer <ck84@mfm015799.local>
2015-10-01 20:55:02 +0100

    Initial Commit

---
+++ ApProgXimateLib/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ApProgXimateLib
+//
+//  Created by Chris Kiefer on 01/10/2015.
+//  Copyright © 2015 Chris Kiefer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
