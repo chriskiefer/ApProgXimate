dfa9050021aed6433f26d44f92e0b5a79f03e9ee
Chris Kiefer <chrisk13fer@gmail.com>
2016-01-14 17:15:52 +0000

    font size changes

--- web/apxFunctions.js
+++ web/apxFunctions.js
@@ -47,12 +47,12 @@ function loadDefaultFunctions() {
   //     }\n \
   //   }\n", 1, 99999);
   //
-  //   appx.addFuncDef("lfsquare", "function lfsquare() {\n \
-  //     this.osc = new Module.maxiOsc();\n \
-  //     this.play = function(j0) {\n \
-  //       return this.osc.square(Module.maxiMap.linexp(j0,-1,1,0.01,15));\n \
-  //     }\n \
-  //   }\n", 1, 99999);
+    appx.addFuncDef("lfsquare", "function lfsquare() {\n \
+      this.osc = new Module.maxiOsc();\n \
+      this.play = function(j0) {\n \
+        return this.osc.square(Module.maxiMap.linexp(j0,-1,1,0.01,15));\n \
+      }\n \
+    }\n", 1, 99999);
   //
   //   appx.addFuncDef("lpf", "function lpf() {\n \
   //     this.filter = new Module.maxiFilter();\n \
@@ -60,19 +60,19 @@ function loadDefaultFunctions() {
   //       return this.filter.lopass(j0, (j1 + 1) / 2);\n \
   //     }\n \
   //   }\n", 2, 99999);
-  //   appx.addFuncDef("lpfz", "function lpfz() {\n \
-  //      this.filter = new Module.maxiFilter();\n \
-  //      this.play = function(j0,j1,j2) {\n \
-  //          return this.filter.lores(j0, Module.maxiMap.linexp(j1,-1,1,20,10000), Module.maxiMap.linlin(j2, -1, 1, 1, 5));\n \
-  //      }\n \
-  //   }\n", 3, 99999);
+    // appx.addFuncDef("lpfz", "function lpfz() {\n \
+    //    this.filter = new Module.maxiFilter();\n \
+    //    this.play = function(j0,j1,j2) {\n \
+    //        return this.filter.lores(j0, Module.maxiMap.linexp(j1,-1,1,20,10000), Module.maxiMap.linlin(j2, -1, 1, 1, 5));\n \
+    //    }\n \
+    // }\n", 3, 99999);
   //
-  //   appx.addFuncDef("hpf", "function hpf() {\n \
-  //      this.filter = new Module.maxiFilter();\n \
-  //      this.play = function(j0,j1) {\n \
-  //          return this.filter.hipass(j0, (j1 + 1) / 2);\n \
-  //      }\n \
-  //   }\n", 2, 99999);
+    appx.addFuncDef("hpf", "function hpf() {\n \
+       this.filter = new Module.maxiFilter();\n \
+       this.play = function(j0,j1) {\n \
+           return this.filter.hipass(j0, (j1 + 1) / 2);\n \
+       }\n \
+    }\n", 2, 99999);
   //
   //   appx.addFuncDef("gate", "function gate() {\n \
   //     this.filter = new Module.maxiFilter();\n \
@@ -85,11 +85,11 @@ function loadDefaultFunctions() {
   //   }\n", 3, 99999);
   //
   //
-  //   appx.addFuncDef("tanh_", "function tanh_() {\n \
-  //     this.play = function(j0) {\n \
-  //       return Math.tanh(j0);\n \
-  //     }\n \
-  //   }\n", 1, 99999);
+    appx.addFuncDef("tanh_", "function tanh_() {\n \
+      this.play = function(j0) {\n \
+        return Math.tanh(j0);\n \
+      }\n \
+    }\n", 1, 99999);
   //
   //   appx.addFuncDef("samp", "\
   // function samp() { \n\
@@ -116,30 +116,30 @@ function loadDefaultFunctions() {
   //        return globalMemory[index];\n \
   //    }\n \
   // }\n", 1, 99999);
-  // appx.addFuncDef("gwr1", "function gwr1() {\n \
-  //   this.play = function(j0) {\n \
-  //     globalMemory[0] = Math.tanh(globalMemory[0] + (j0)); \
-  //     return j0;\n \
-  //   }\n \
-  // }\n", 1, 99999);
+  appx.addFuncDef("gwr1", "function gwr1() {\n \
+    this.play = function(j0) {\n \
+      globalMemory[0] = Math.tanh(globalMemory[0] + (j0)); \
+      return j0;\n \
+    }\n \
+  }\n", 1, 99999);
+
+  appx.addFuncDef("gr1", "function gr1() {\n \
+    this.play = function(j0) {\n \
+      return globalMemory[0] * j0;\n \
+    }\n \
+  }\n", 1, 99999);
   //
-  // appx.addFuncDef("gr1", "function gr1() {\n \
-  //   this.play = function(j0) {\n \
-  //     return globalMemory[0] * j0;\n \
-  //   }\n \
-  // }\n", 1, 99999);
-  //
-  // appx.addFuncDef("seq", "function seq() {\n \
-  //   this.vals = [1,9,4,-6,3]; \
-  //   this.idx = 0; \
-  //   this.last =0; \
-  //    this.play = function(j0) {\n \
-  //        if (this.last < 0 && j0 >= 0) {\
-  //          this.idx++; \
-  //          if (this.idx == this.vals.length) this.idx = 0; \
-  //        } \
-  //        this.last = j0; \
-  //        return this.vals[this.idx] / 10.0;\n \
-  //    }\n \
-  // }\n", 1, 99999);
+  appx.addFuncDef("seq", "function seq() {\n \
+    this.vals = [1,9,4,-6,3]; \
+    this.idx = 0; \
+    this.last =0; \
+     this.play = function(j0) {\n \
+         if (this.last < 0 && j0 >= 0) {\
+           this.idx++; \
+           if (this.idx == this.vals.length) this.idx = 0; \
+         } \
+         this.last = j0; \
+         return this.vals[this.idx] / 10.0;\n \
+     }\n \
+  }\n", 1, 99999);
 }
