f8f3ef6350e3b5e2539aec9e733af8b2bea9cf16
Chris <chris@luuma.net>
2016-01-15 23:05:22 +0000

    save and load scenes locally and online

--- web/api/index.php
+++ web/api/index.php
@@ -13,7 +13,7 @@ $app = new \Slim\App;
 $app->get('/scenes', function (Request $request, Response $response) {
     //$name = $request->getAttribute('name');
     $conn = mysqlConnect();
-    $sth = $conn->query("SELECT id, nickname, title, description from scenes");
+    $sth = $conn->query("SELECT id, nickname as nickName, title, description as 'desc' from scenes");
     $rows = array();
     while($r = mysqli_fetch_assoc($sth)) {
         $rows[] = $r;
@@ -26,7 +26,7 @@ $app->get('/scene/{id}', function (Request $request, Response $response) {
     $id = $request->getAttribute('id');
     $conn = mysqlConnect();
     $sth = $conn->query("UPDATE scenes SET accessCount = accessCount + 1, lastAccess = NOW() where id = $id");
-    $sth = $conn->query("SELECT id, nickname, title, description, jsondata from scenes where id = $id");
+    $sth = $conn->query("SELECT id, nickname as nickName, title, description as 'desc', jsondata from scenes where id = $id");
     $rows = array();
     while($r = mysqli_fetch_assoc($sth)) {
         $rows[] = $r;
